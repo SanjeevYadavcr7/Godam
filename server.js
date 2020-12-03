f75e5660bd9f8420909f88ad0c1d1112ce01380c
Sanjeev Yadav <sanjeevyadavcr7@gmail.com>
2020-12-03 12:02:12 +0530

    Procfile added

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const Emitter = require('events');
 
 
 // Database Connection
-mongoose.connect(process.env.MONGO_CONNECTION_URL, { useNewUrlParser: true, useCreateIndex:true, useUnifiedTopology:true, useFindAndModify:true}
+mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useCreateIndex:true, useUnifiedTopology:true, useFindAndModify:true}
     );
 const connection = mongoose.connection;
 connection.once('open',() => {
